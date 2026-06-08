1a0dc5db4e3ebd06f1b6724de8c0b66539f8980e
Oblutack <kamenjas.evvel@gmail.com>
2026-06-08 17:40:54 +0200

    Configure local Postgres and build Tailwind landing page

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+# config/routes.rb
+Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "home#index"
+  
+  # You can delete the get 'home/index' line that the generator made
+end
