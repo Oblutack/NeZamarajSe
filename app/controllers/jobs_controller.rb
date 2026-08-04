8e8794d2c37357b119a3df48eb025041d5a0bc7e
Oblutack <kamenjas.evvel@gmail.com>
2026-08-04 23:52:35 +0200

    Implement UserPreferences, filtering engine, and AI badges on Job Market

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,24 @@
 # app/controllers/jobs_controller.rb
 class JobsController < ApplicationController
-  # We want users to log in to see the job market
   before_action :authenticate_user!
 
   def index
-    # Fetch all jobs, include the company data to prevent N+1 queries, order by newest
+    @saved_job_ids = current_user.applications.pluck(:job_id)
+    preference = current_user.user_preference
+
+    # Start with all jobs
     @jobs = Job.includes(:company).order(created_at: :desc)
 
-    # We also want to know which jobs the user has ALREADY saved, so we can hide the "Save" button
-    @saved_job_ids = current_user.applications.pluck(:job_id)
+    # If the user has keywords, apply the SQL filter
+    if preference && preference.keyword_array.any?
+      # This dynamically generates a SQL query like:
+      # title ILIKE '%developer%' OR title ILIKE '%software%'
+      conditions = preference.keyword_array.map { |kw| "title ILIKE ?" }.join(" OR ")
+      values = preference.keyword_array.map { |kw| "%#{kw}%" }
+
+      @jobs = @jobs.where(conditions, *values)
+    end
+
+    # TODO: In the future, we can add location filtering here too!
   end
 end
